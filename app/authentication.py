682f3bb2f78d42e6c9a3012dd194f742404bb1ae
Daniel Muñoz Gonzalez <dani16595@gmail.com>
2023-05-07 12:15:25 +0200

    update auth

--- app/authentication.py
+++ app/authentication.py
@@ -36,13 +36,8 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-async def get_user(name: str, db: Session):
-    item = await User.get_name(name=name, db=db)
-    return item
-
-
-def authenticate_user(name: str, password: str, db):
-    user = get_user(name=name, db=db)
+async def authenticate_user(name: str, password: str, db) -> User:
+    user = await User.get_name(name=name, db=db)
     print(f"USER: {user}")
     if not user:
         return False
@@ -63,8 +58,8 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     return encoded_jwt
 
 
-def generate_token(name, password, db):
-    user = authenticate_user(name, password, db)
+async def generate_token(name, password, db):
+    user = await authenticate_user(name, password, db)
     print(user)
     if not user:
         raise HTTPException(
@@ -72,27 +67,28 @@ def generate_token(name, password, db):
             detail="Incorrect email/username or password",
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    return create_access_token(
+    token = create_access_token(
         data={"sub": user.name}, expires_delta=access_token_expires
     )
+    return token
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-
-    user = get_user(username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
+# async def get_current_user(token: str = Depends(oauth2_scheme)):
+#     credentials_exception = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Could not validate credentials",
+#     )
+#     try:
+#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+#         username: str = payload.get("sub")
+#         if username is None:
+#             raise credentials_exception
+#         token_data = TokenData(username=username)
+#     except JWTError:
+#         raise credentials_exception
+#
+#     user = get_user(username=token_data.username, db=db)
+#     if user is None:
+#         raise credentials_exception
+#     return user
 
