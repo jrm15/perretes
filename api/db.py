2ceece66d84a3f9a5f41f2ac845d7ad60eab95f5
Daniel Muñoz Gonzalez <dani16595@gmail.com>
2023-08-03 19:24:11 +0200

    create connection database cockroach

--- api/db.py
+++ api/db.py
@@ -1,10 +1,18 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker
+from api.settings import app_settings
 
 Base = declarative_base()
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+if app_settings.DB_HOST:
+    SQLALCHEMY_DATABASE_URL = URL.create("cockroachdb", username=app_settings.DB_USER,
+                                         password=app_settings.DB_PASSWORD, host=app_settings.DB_HOST,
+                                         port=app_settings.DB_PORT, database=app_settings.DB_NAME,
+                                         query={"sslmode": "disable"})
+else:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- api/settings.py
+++ api/settings.py
@@ -8,3 +8,10 @@ class Settings(BaseSettings):
 
     secret_key: str = os.getenv('SECRET_KEY')
     token_expire: int = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+    DB_HOST: str = None
+    DB_USER: str = None
+    DB_PASSWORD: str = None
+    DB_PORT: str = None
+    DB_NAME: str = None
+
+app_settings = Settings()
